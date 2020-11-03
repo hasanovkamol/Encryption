bad34e5690779aceac38fe97eec378f9f97c32c9
Kamol Hasanov <hasanovkamol3834@mail.ru>
2020-11-03 13:27:15 +0500

    Add project files.

---
+++ Encryption/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.matin = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.htmlLabel1 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.htmlLabel2 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.htmlLabel3 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.htmlLabel4 = new MetroFramework.Drawing.Html.HtmlLabel();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // matin
+            // 
+            this.matin.Location = new System.Drawing.Point(49, 78);
+            this.matin.Name = "matin";
+            this.matin.Size = new System.Drawing.Size(270, 45);
+            this.matin.TabIndex = 0;
+            this.matin.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(49, 224);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(270, 124);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "Sezir Encryption",
+            "Polibiy"});
+            this.metroComboBox1.Location = new System.Drawing.Point(49, 149);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(128, 29);
+            this.metroComboBox1.TabIndex = 2;
+            this.metroComboBox1.UseSelectable = true;
+            this.metroComboBox1.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(239, 149);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(80, 29);
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "Encryption";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // htmlLabel1
+            // 
+            this.htmlLabel1.AutoScroll = true;
+            this.htmlLabel1.AutoScrollMinSize = new System.Drawing.Size(32, 23);
+            this.htmlLabel1.AutoSize = false;
+            this.htmlLabel1.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel1.Location = new System.Drawing.Point(49, 53);
+            this.htmlLabel1.Name = "htmlLabel1";
+            this.htmlLabel1.Size = new System.Drawing.Size(52, 23);
+            this.htmlLabel1.TabIndex = 4;
+            this.htmlLabel1.Text = "Text";
+            // 
+            // htmlLabel2
+            // 
+            this.htmlLabel2.AutoScroll = true;
+            this.htmlLabel2.AutoScrollMinSize = new System.Drawing.Size(35, 23);
+            this.htmlLabel2.AutoSize = false;
+            this.htmlLabel2.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel2.Location = new System.Drawing.Point(49, 124);
+            this.htmlLabel2.Name = "htmlLabel2";
+            this.htmlLabel2.Size = new System.Drawing.Size(52, 23);
+            this.htmlLabel2.TabIndex = 5;
+            this.htmlLabel2.Text = "Type";
+            // 
+            // htmlLabel3
+            // 
+            this.htmlLabel3.AutoScroll = true;
+            this.htmlLabel3.AutoScrollMinSize = new System.Drawing.Size(76, 23);
+            this.htmlLabel3.AutoSize = false;
+            this.htmlLabel3.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel3.Location = new System.Drawing.Point(49, 198);
+            this.htmlLabel3.Name = "htmlLabel3";
+            this.htmlLabel3.Size = new System.Drawing.Size(89, 23);
+            this.htmlLabel3.TabIndex = 6;
+            this.htmlLabel3.Text = "Code on Text";
+            // 
+            // htmlLabel4
+            // 
+            this.htmlLabel4.AutoScroll = true;
+            this.htmlLabel4.AutoScrollMinSize = new System.Drawing.Size(29, 23);
+            this.htmlLabel4.AutoSize = false;
+            this.htmlLabel4.BackColor = System.Drawing.SystemColors.Window;
+            this.htmlLabel4.Location = new System.Drawing.Point(176, 124);
+            this.htmlLabel4.Name = "htmlLabel4";
+            this.htmlLabel4.Size = new System.Drawing.Size(58, 23);
+            this.htmlLabel4.TabIndex = 7;
+            this.htmlLabel4.Text = "Key";
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(29, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(183, 149);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(51, 23);
+            this.metroTextBox1.TabIndex = 8;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.metroTextBox1.Click += new System.EventHandler(this.metroTextBox1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(239, 188);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(80, 29);
+            this.metroButton2.TabIndex = 9;
+            this.metroButton2.Text = "unEncryption";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 370);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.htmlLabel4);
+            this.Controls.Add(this.htmlLabel3);
+            this.Controls.Add(this.htmlLabel2);
+            this.Controls.Add(this.htmlLabel1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.matin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox matin;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel1;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel2;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel3;
+        private MetroFramework.Drawing.Html.HtmlLabel htmlLabel4;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ Encryption/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encryption
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            metroTextBox1.Visible = false;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+           
+            if(metroComboBox1.Text== "Sezir Encryption")
+            {
+                richTextBox2.Text = SezirKey(matin.Text.ToUpper(), int.Parse(metroTextBox1.Text));
+            }
+            else if("Polibiy" == metroComboBox1.Text)
+            {
+                richTextBox2.Text = PloibiyaLog(matin.Text.ToUpper());
+            }
+        }
+        
+       /// <summary>
+       /// Ecryption and unencrption For Sezir Log
+       /// </summary>
+       /// <param name="matin"></param>
+       /// <param name="key"></param>
+       /// <returns></returns>
+        private string SezirKey(string matin, int key)
+        {
+            string keymatin = "";
+            foreach (var item in matin)
+            {
+                if ((int)item>=65 && (int)item<=91)
+                {
+
+                    if(key>0)
+                    {
+                        if ((91 - (int)item) >= key)
+                        {
+                            keymatin += (char)((int)item + key);
+                        }
+                        else
+                        {
+                            keymatin += (char)(65 + (key - (91 - (int)item)));
+                        }
+                    }
+                    else
+                    {
+                        if((int)item-65>=Math.Abs(key))
+                        {
+                            keymatin += (char)((int)item + key);
+                        }
+                        else
+                        {
+                            keymatin += (char)(91 + ((int)item - 65)+key);
+                        }
+                    }
+                }
+                else
+                {
+                    keymatin += item;
+                }
+               
+            }
+            return keymatin;
+        }
+
+
+        /// <summary>
+        /// Encryption for Ploibiy Log
+        /// </summary>
+        /// <param name="PloibiyLog"></param>
+        /// <param name="matin"></param>
+        /// 
+        private string PloibiyaLog(string matin)
+        {
+            int i = 0, j = 0;
+            string keymatin = "";
+            foreach (var item in matin)
+            {
+                if ((int)item>=65 && (int)item<=90)
+                {
+
+                    i = ((int)item - 65) / 5;
+                    j = ((int)item - 65) % 5;
+                    keymatin += (char)(65 + i);
+                    keymatin += (char)(65 + j);
+                }
+                else
+                {
+                    keymatin += item;
+                }
+
+            }
+
+            return keymatin;
+        }
+
+        /// <summary>
+        /// unencryption for Ploibiya Log
+        /// </summary>
+        /// <param name="PloibyLog"></param>
+        /// <param name="mation"></param>
+        private string UnPloibyaLog(string matin)
+        {
+            int i = 0, j = 0;
+            int k = 0;
+            string keymatin = "";
+            foreach (var item in matin)
+            {
+                if ((int)item >= 65 && (int)item <= 90)
+                {
+                    k++;
+                    i = (int)item - 65;
+                    if (k==2)
+                    {
+                        k = 0;
+                        keymatin += (char)(65 + j * 5 + i);
+                    }
+                    else
+                    {
+                        j = i;
+                    }
+
+                }
+                else
+                {
+                    keymatin += item;
+                }
+            }
+
+            return keymatin;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(metroComboBox1.Text=="Sezir Encryption")
+            {
+                metroTextBox1.Visible = true;
+                metroTextBox1.Text = "0";
+            }
+            else
+            {
+                metroTextBox1.Visible = false;
+            }
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+
+            if (metroComboBox1.Text == "Sezir Encryption")
+            {
+                richTextBox2.Text = SezirKey(richTextBox2.Text, (-1)* int.Parse(metroTextBox1.Text));
+            }
+            else if(metroComboBox1.Text== "Polibiy")
+            {
+                richTextBox2.Text = UnPloibyaLog(richTextBox2.Text);
+            }
+        }
+    }
+}
